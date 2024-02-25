0b345b0b0512736f32d9ccdf1874448b24f1f55e
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-25 15:36:16 +0800

    Overridden save method in user model
    Added functionality to hash the password before saving it to the database in the save method of the user model. This is done to enhance security by ensuring that passwords of users created through the admin panel are not stored in plain text.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,6 +10,13 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def save(self, *args, **kwargs):
+        # Проверяем, что пароль был изменен или это новый пользователь
+        if self._state.adding or 'password' in kwargs:
+            # Хешируем пароль
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.email}, {self.first_name} {self.last_name}'
