558ff9fd35635bdd0ba48513267ceda8a62c4916
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-25 19:47:58 +0800

    Configured routing for the 'products' application

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # Apps
+    path('api/products/', include('products.urls', namespace='products')),
+    # path('api/suppliers/', include('suppliers.urls', namespace='suppliers')),
+
+    # todo А нужны ли вообще урлы пользователю ???
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
