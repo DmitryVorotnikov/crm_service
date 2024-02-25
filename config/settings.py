2f283884b907a50b6c6557357d554d1aee7c197f
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-25 14:21:12 +0800

    Added environment variable for specifying project runtime environment

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')
 
+# Type of environment
+ENV_TYPE = os.getenv('ENV_TYPE')  # local, test, stage, prod
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
