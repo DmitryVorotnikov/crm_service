be1c58f089defa8d4d4207899d9d1e02edef8064
DmitryVorotnikov <vorotnikov7009@gmail.com>
2024-02-25 14:32:38 +0800

    Added configurations for handling static and media

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if ENV_TYPE == 'local':
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
